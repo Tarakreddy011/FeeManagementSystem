14eda848f6c8999e361ed14276214afe9a9d75ce
Singamreddy Tarakeswar Reddy <singamtarak011@gmail.com>
2026-08-24 21:37:15 +0530

    spring security class

---
+++ src/main/java/com/fms/demoFMS/Security/SpringSecurity.java
@@ -0,0 +1,10 @@
+package com.fms.demoFMS.Security;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SpringSecurity {
+}
